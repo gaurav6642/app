3614167628c17c809dd91e7daab98955d4c2d76d
Gaurav <gaurav6642@gmail.com>
2019-06-11 20:24:37 +0530

    Initial commit

---
+++ app/src/main/java/com/example/jayvijay/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.jayvijay;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView wv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+        wv = findViewById(R.id.web_view);
+
+        wv.loadUrl("http://rohitomjewellers.com");
+        wv.setWebViewClient(new WebViewClient());
+        WebSettings ws = wv.getSettings();
+        ws.setJavaScriptEnabled(true);
+        wv.setWebChromeClient(new WebChromeClient());
+
+    }
+
+    public void onBackPressed() {
+
+        if (wv.canGoBack()) {
+            wv.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
+
